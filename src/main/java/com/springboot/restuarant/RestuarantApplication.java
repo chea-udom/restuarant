e8e13926f00d1f3952eb92d52191bfc72d42dc3b
Chea Udom <chea_udom@ifinanceplc.com>
2022-08-26 16:30:41 +0700

    add

--- src/main/java/com/springboot/restuarant/RestuarantApplication.java
+++ src/main/java/com/springboot/restuarant/RestuarantApplication.java
@@ -9,6 +9,7 @@ public class RestuarantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestuarantApplication.class, args);
 		System.out.print("hello from local");
+		System.out.print("hello from local");
 	}
 
 }
