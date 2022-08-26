355cc6ba138a3b24dc6697546eadab5ed4d64e7f
Chea Udom <chea_udom@ifinanceplc.com>
2022-08-26 15:53:55 +0700

    init project

---
+++ src/test/java/com/springboot/restuarant/RestuarantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restuarant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestuarantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
